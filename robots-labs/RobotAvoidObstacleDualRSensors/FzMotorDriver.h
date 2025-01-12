1e57dfa836dc6aa51cceb1c58691c16c0bc0bc65
Eduardo Fritzen <fritzen@gmail.com>
2025-01-12 13:58:17 -0300

    +

--- robots-labs/RobotAvoidObstacleDualRSensors/FzMotorDriver.h
+++ robots-labs/RobotAvoidObstacleDualRSensors/FzMotorDriver.h
@@ -1,4 +1,4 @@
- /* ---------------------------------------------------------------------------------------
+/* ---------------------------------------------------------------------------------------
  * Autor / Author: Eduardo Fritzen eduardo@fritzen.io
  * Não é permitida o uso para fins comerciais deste código sem a autrorização por escrito do autor
  * Use of this code for commercial purposes is not permitted without written authorization from the author
@@ -11,45 +11,15 @@
 #include <stdio.h>
 #include <inttypes.h>
 
-#include <Servo.h>
-
-#define DEFAULT_SPEED 125 // até 255
-#define SERVO_STOP 90
-#define SERVO_MAX 120
-#define SERVO_MIN 60
-
-
-//L293D - FECHADO DE ACORDO COM A PLACA SHIELD  
-#define DEFAULT_MOTOR_PIN_1 10 //PWM
-#define DEFAULT_MOTOR_PIN_2 12
-
-#define DEFAULT_MOTOR_PIN_3 11 //PWM
-#define DEFAULT_MOTOR_PIN_4 13
-
-//declaracao dos pinos utilizados para controlar a velocidade de rotacao
-#define DEFAULT_MOTOR_ENA_PIN 5
-#define DEFAULT_MOTOR_ENB_PIN 6
-
-
-// Servo
-#define DEFAULT_SERVO_MOTOR_A_PIN 10
-#define DEFAULT_SERVO_MOTOR_B_PIN 12
-
-
-#define SENSOR_THRESHOLD 400
-
-enum TipoMotor {
-    L9110,
-    L293,
-    SERVO
-};
-
+// L293D - Pinos padrão para IN1, IN2, IN3, IN4
+#define DEFAULT_MOTOR_IN1 4  // Motor A - Input 1
+#define DEFAULT_MOTOR_IN2 5  // Motor A - Input 2
+#define DEFAULT_MOTOR_IN3 6  // Motor B - Input 3
+#define DEFAULT_MOTOR_IN4 7  // Motor B - Input 4
 
 class FzMotorDriver
 {
     public:
-        
-
         FzMotorDriver();
         void forward();
         void backward();
@@ -58,38 +28,15 @@ class FzMotorDriver
         void turnRightAxis();
         void turnLeftAxis();
         void stop();
-        void test();
         
-        void attach(TipoMotor _tipo);
-        void attach(uint8_t pinServo1, uint8_t pinServo2);
-        void attach(uint8_t pin1MA, uint8_t pinPwmA, uint8_t pin1MB, uint8_t pinPwmB);
-        void attach(uint8_t pin1A, uint8_t pin2A, uint8_t pin1B, uint8_t pin2B, uint8_t pinENMA, uint8_t pinENMB);
-        
-
-        void setSpeed(uint16_t speed1, uint16_t speed2);
-        
+        // Novo método attach simplificado para L293D
+        void attach(uint8_t in1, uint8_t in2, uint8_t in3, uint8_t in4);
  
     private:
-         
-        Servo _servoA;
-        Servo _servoB;
-
-        uint8_t _pin1MA;
-        uint8_t _pin2MA; //pode não ter
-        uint8_t _pin1MB;
-        uint8_t _pin2MB; //pode não ter
-
-        uint8_t _pinPwmA; //ENMA
-        uint8_t _pinPwmB; //ENMB
-
-        uint8_t _pinServoA;
-        uint8_t _pinServoB;
-
-        uint16_t _speedA = 255;
-        uint16_t _speedB = 255;
-
-        TipoMotor _tipo = L9110;
-
+        uint8_t _pin1MA; // IN1
+        uint8_t _pin2MA; // IN2
+        uint8_t _pin1MB; // IN3
+        uint8_t _pin2MB; // IN4
 };
  
 #endif
--- robots-labs/RobotAvoidObstacleDualRSensors/RobotAvoidObstacleDualRSensors.ino
+++ robots-labs/RobotAvoidObstacleDualRSensors/RobotAvoidObstacleDualRSensors.ino
@@ -11,27 +11,29 @@ const int MOTOR_DIR_B = 7;  // Direction Motor 2
 const int SENSOR_IR_ESQ = A6;  // Sensor IR esquerdo no pino analógico A0
 const int SENSOR_IR_DIR = A7;  // Sensor IR direito no pino analógico A1
 
-const int BUZZER = 4;
+const int BUZZER = 8;
 
 // Define o valor de threshold para detectar obstáculos
 // Quanto menor o valor analógico, mais próximo está o obstáculo
-const int THRESHOLD = 100;  // Ajuste este valor conforme a necessidade do seu sensor
+const int THRESHOLD = 500;  // Ajuste este valor conforme a necessidade do seu sensor
 
 FzMotorDriver motor;
 
 void setup() {
-  // Definição dos pinos como entrada
   pinMode(SENSOR_IR_ESQ, INPUT);
   pinMode(SENSOR_IR_DIR, INPUT);
   pinMode(BUZZER, OUTPUT);
   
-  // Inicializa a comunicação serial para depuração
   Serial.begin(9600);
-
   Serial.println("SETUP");
   
   motor.attach(MOTOR_DIR_A, MOTOR_PWM_A, MOTOR_DIR_B, MOTOR_PWM_B);
-  motor.setSpeed(120, 100);
+}
+
+void alertBuzzer() {
+  digitalWrite(BUZZER, HIGH);
+  delay(100);
+  digitalWrite(BUZZER, LOW);
 }
 
 void loop() {
@@ -45,35 +47,34 @@ void loop() {
   Serial.print(" | Dir: "); 
   Serial.println(valorSensorDir);
 
-  // Lógica de evitar obstáculos
-  // Note que para sensores IR, valores menores indicam objetos mais próximos
-  if (valorSensorEsq < THRESHOLD && valorSensorDir < THRESHOLD) {
-    // Obstáculo detectado em ambos os sensores
-    motor.backward();
-    delay(600);
-    motor.turnRightAxis();
-    delay(1000);
-    motor.stop();
-    digitalWrite(BUZZER, HIGH);
-    delay(500);
-    digitalWrite(BUZZER, LOW);
-    delay(500);
-    digitalWrite(BUZZER, HIGH);
-    delay(500);
-    digitalWrite(BUZZER, LOW);
-  } 
-  else if (valorSensorEsq > THRESHOLD && valorSensorDir > THRESHOLD) {
-    // Nenhum obstáculo detectado
+  bool obstaculoEsq = (valorSensorEsq <= THRESHOLD);
+  bool obstaculoDir = (valorSensorDir <= THRESHOLD);
+
+  if (!obstaculoEsq && !obstaculoDir) {
+    // Nenhum obstáculo - segue em frente
     motor.forward();
   } 
-  else if (valorSensorEsq < valorSensorDir) {
-    // Obstáculo mais próximo à esquerda
-    motor.turnRight();
-  } 
-  else {
-    // Obstáculo mais próximo à direita
-    motor.turnLeft();
+  else if (obstaculoEsq && obstaculoDir) {
+    // Obstáculo em ambos os lados
+    alertBuzzer();
+    motor.backward();
+    delay(800);  // Recua por mais tempo
+    motor.turnRightAxis();
+    delay(1000); // Gira 90 graus
+    motor.stop();
+  }
+  else if (obstaculoEsq) {
+    motor.backward();
+    delay(50);  // Recua por mais tempo
+    motor.turnLeft();  // Vira para direita
+    delay(100);
+  }
+  else if (obstaculoDir) {
+    motor.backward();
+    delay(50);  // Recua por mais tempo
+    motor.turnRight();   // Vira para esquerda
+    delay(100);
   }
   
-  delay(5); // Pequeno atraso para evitar oscilações
+
 }
